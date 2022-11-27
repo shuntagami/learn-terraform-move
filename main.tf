3e18e0e2e9b337fd9eb7994422694e2b433c0b3c
shuntagami <shuntagami23@gmail.com>
2022-11-27 18:09:40 +0900

    Refactoring with compute module

--- main.tf
+++ main.tf
@@ -29,43 +29,38 @@ module "vpc" {
   tags = var.vpc_tags
 }
 
-module "security_group" {
-  source = "./modules/security_group"
+module "web_security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
+
   vpc_id = module.vpc.vpc_id
+
+  use_name_prefix = false
+
+  name        = "terraform-learn-move-sg"
+  description = "Security Group managed by Terraform"
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "example" {
-  ami                         = data.aws_ami.ubuntu.id
-  subnet_id                   = module.vpc.public_subnets[0]
-  instance_type               = "t2.micro"
-  vpc_security_group_ids      = [module.security_group.security_group_id]
-  associate_public_ip_address = true
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
-  tags = {
-    Name = "terraform-learn-move-ec2"
-  }
+module "ec2_instance" {
+  source         = "./modules/compute"
+  security_group = module.web_security_group.security_group_id
+  public_subnets = module.vpc.public_subnets
 }
